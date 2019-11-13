ffa1fe8dbd3141b79c9bb4f4fe16211e9648d3ff
algebraic-designer-6218 <jmiller@archersystems.us>
2019-11-13 16:43:39 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,11 @@ def toadstool
   status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
+def link 
+  phrase =  "It's Dangerous To Go Alone! Take This."
+  puts phrase
+end
+def any_phrase
+  phrase= "Do A Barrel Roll!"
+  puts phrase
+end
